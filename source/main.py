85ab6bfb98daae3012155ac5fe3be5e3c37562f2
Dtzz81 <denise.turkova@madetech.com>
2025-02-21 13:31:03 +0000

    test one brace is missing

--- source/main.py
+++ source/main.py
@@ -1,16 +1,21 @@
 def wild_braces(string):
-    counter = 0
+    counter_p = 0
     for i in string:
         if i == '(':
-            counter += 1
+            counter_p += 1
         elif i == ')':
-            counter -= 1
+            counter_p -= 1
 
-    if counter == 0:
+    counter_b = 0
+    for i in string:
+        if i == '[':
+            counter_b += 1
+        elif i == ']':
+            counter_b -= 1
+
+
+    if counter_p == 0 and counter_b == 0:
         return True
     else:
         return False
-
-
-
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -44,3 +44,25 @@ def test_there_are_2_pairs_of_parentheses_and_braces():
 
     #assert
     assert result == expected
+
+def test_there_are_2_pairs_of_mixed_parentheses_and_braces():
+    #arange
+    example = "([)]"
+
+    #act
+    expected = True
+    result = wild_braces(example)
+
+    #assert
+    assert result == expected
+
+def test_one_brace_is_missing():
+    #arange
+    example = "([)"
+
+    #act
+    expected = False
+    result = wild_braces(example)
+
+    #assert
+    assert result == expected
